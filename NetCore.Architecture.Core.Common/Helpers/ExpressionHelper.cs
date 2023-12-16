f0c20453b45db51878cb003c304d953bd15be95f
duykasama <duykasama@gmail.com>
2023-12-16 19:31:50 +0700

    add more helpers

---
+++ NetCore.Architecture.Core.Common/Helpers/ExpressionHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+
+namespace NetCore.Architecture.Core.Common.Helpers;
+
+public static class ExpressionHelper
+{
+    public static Expression<Func<T, bool>> CombineExpressionsWithOr<T>(Expression<Func<T, bool>> left,
+        Expression<Func<T, bool>> right)
+    {
+        var param = Expression.Parameter(typeof(T), typeof(T).Name);
+
+        var combinedExpr = Expression.Or(
+            Expression.Invoke(left, param),
+            Expression.Invoke(right, param));
+
+        return Expression.Lambda<Func<T, bool>>(combinedExpr, param);
+    }
+    
+    public static Expression<Func<T, bool>> CombineExpressionsWithAnd<T>(Expression<Func<T, bool>> left,
+        Expression<Func<T, bool>> right)
+    {
+        var param = Expression.Parameter(typeof(T), typeof(T).Name);
+
+        var combinedExpr = Expression.And(
+            Expression.Invoke(left, param),
+            Expression.Invoke(right, param));
+
+        return Expression.Lambda<Func<T, bool>>(combinedExpr, param);
+    }
+}
