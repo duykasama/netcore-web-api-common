5e6e507b621eba4ac867a60c0f3779e8269e3fc6
duykasama <duykasama@gmail.com>
2023-12-16 22:30:10 +0700

    add IDependencyProvider implementation

---
+++ NetCore.WebApiCommon.Infrastructure.Common/Implementations/DependencyProvider.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using NetCore.WebApiCommon.Core.Common.Interfaces;
+
+namespace NetCore.WebApiCommon.Infrastructure.Common.Implementations;
+
+public class DependencyProvider : GenericService, IDependencyProvider
+{
+    private readonly ILifetimeScope _scope;
+
+    public DependencyProvider(ILifetimeScope scope) : base(scope)
+    {
+        _scope = scope;
+    }
+
+    public T ResolveService<T>()
+    {
+        return _scope.Resolve<T>();
+    }
+
+    public object ResolveService(Type serviceType)
+    {
+        return _scope.Resolve(serviceType);
+    }
+}
