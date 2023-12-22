62028c300f07949e56a1ef0739e667011d4ed760
duykasama <duykasama@gmail.com>
2023-12-22 20:55:27 +0700

    udpate UseSwagge() and UseCors()

--- NetCore.WebApiCommon.Infrastructure/Extensions/WebApplicationExtensions.cs
+++ NetCore.WebApiCommon.Infrastructure/Extensions/WebApplicationExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using NetCore.WebApiCommon.Core.Common.Models;
 using NetCore.WebApiCommon.Core.Constants;
-using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace NetCore.WebApiCommon.Infrastructure.Extensions;
 
@@ -10,36 +9,34 @@ public static class WebApplicationExtensions
     public static void UseSwaggerInEnvironments(this WebApplication app, params string[] environments)
     {
         if (!environments.Contains(GlobalData.CurrentEnvironment)) return;
-     
-        UseSwaggerInEnvironmentInternal(app);
+        
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.DisplayRequestDuration();
+            options.EnableTryItOutByDefault();
+        });
     }
     
     public static void UseSwaggerInEnvironments(this IApplicationBuilder app, params string[] environments)
     {
         if (!environments.Contains(GlobalData.CurrentEnvironment)) return;
         
-        UseSwaggerInEnvironmentInternal(app);
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.DisplayRequestDuration();
+            options.EnableTryItOutByDefault();
+        });
     }
 
-    public static void UseAppCors(this WebApplication app)
+    public static void UseDefaultCorsPolicy(this WebApplication app)
     {
         app.UseCors(CorsConstants.APP_CORS_POLICY);
     }
     
-    public static void UseAppCors(this IApplicationBuilder app)
+    public static void UseDefaultCorsPolicy(this IApplicationBuilder app)
     {
         app.UseCors(CorsConstants.APP_CORS_POLICY);
-    }
-
-    private static void UseSwaggerInEnvironmentInternal(dynamic app)
-    {
-        Action<SwaggerUIOptions> action = options =>
-        {
-            options.DisplayRequestDuration();
-            options.EnableTryItOutByDefault();
-        };
-        
-        app.UseSwagger();
-        app.UseSwaggerUI(action);
     }
 }
